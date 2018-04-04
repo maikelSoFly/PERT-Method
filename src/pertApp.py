9f9b3b68a0b29a9c67ab949c30647ed0f889b635
staho <stacho688@gmail.com>
2018-04-04 19:04:02 +0200

    Functions rework
    Variation is calculated and saved in object

--- src/pertApp.py
+++ src/pertApp.py
@@ -8,18 +8,36 @@ def readData(dataName):
 
 def calculateExpected(times):
     numerator = times["tc"] + 4 * times["tm"] + times["tp"]
-    return numerator/6
+    times["expected"] = numerator/6
+    return times["expected"]
 
-def calculateVariation(times):
-    numerator = times["tp"] - times["tc"]
-    return (numerator/6)**2
+def calculateVariation(tasks):
+    for key, task in tasks.items():
+        times = task["times"]
+        numerator = times["tp"] - times["tc"]
+        task["variation"] = (numerator/6)**2
+
 
 def standardDeviation(times):
     numerator = times["tp"] + times["tc"]
     return numerator/6
+
+# def 
+
+# def processForward(tasks):
+
+# def processBackward(tasks):
+
+# def postProcess(tasks):
 
 if __name__ == '__main__':
     taskData = readData("tasks.json")
     for key, value in taskData.items():
         print(calculateExpected(value["times"]))
         value["timeStart"] = 6.
+    print(taskData)
+
+
+# docs
+# https://mfiles.pl/pl/index.php/PERT
+# https://4business4you.com/biznes/zarzadzanie-projektami/metoda-pert-w-praktyce/
