70f2a11a34263e74723bc42729fa3cfcccbfb12c
maikelSoFly <mikolaj.stepniewski1@gmail.com>
2018-04-04 20:31:02 +0200

    Forward analysis

--- src/pertApp.py
+++ src/pertApp.py
@@ -30,20 +30,25 @@ def processForward(tasks):
     for taskId, task in tasks.items():
         previousTasksIds = task["previous"]
         minStart = 0
+        minEnd = 0
         times = task["times"]
 
         if len(previousTasksIds) == 1:
             minStart = tasks[previousTasksIds[0]]["times"]["tm"]
+
         if len(previousTasksIds) >= 2:
             prevTasksTms = [tasks[id]['times']['tm']
                             for id in previousTasksIds]
             minStart = max(prevTasksTms)
 
         times["minStart"] = minStart
+        times['minEnd'] = minStart + times['tm']
+
 
 # def processBackward(tasks):
+#     for taskId, task in tasks.items():
 
-# def postProcess(tasks):
+        # def postProcess(tasks):
 
 
 if __name__ == '__main__':
@@ -51,7 +56,7 @@ if __name__ == '__main__':
     taskData = readData("tasks.json")
     processForward(taskData)
     for key, value in taskData.items():
-        print(key, value['times']['minStart'])
+        print(key, value['times']['minStart'], value['times']['minEnd'])
 
         # print(calculateExpected(value["times"]))
         # value["timeStart"] = 6.
